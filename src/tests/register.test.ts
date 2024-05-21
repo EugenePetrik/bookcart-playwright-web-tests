c75c6a8e53233dfc052ea0be3a9dfb644ca96d7e
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-05-21 08:24:49 +0300

    Add register test

---
+++ src/tests/register.test.ts
@@ -0,0 +1,15 @@
+import { baseFixture as test } from '../fixtures';
+import UserBuilder from '../utils/models/UserBuilder';
+
+test(
+  'user should be able to registered successfully',
+  { tag: '@ui' },
+  async ({ app }) => {
+    await app.registerPage.open();
+
+    const user = new UserBuilder().build();
+    await app.registerPage.registerUser(user);
+
+    await app.loginPage.expectURL(/login/);
+  },
+);
